2053f7a805d0e458e5f3844e4cf7472653d86d40
ThuanPham777 <phamchithuan777@gmail.com>
2024-11-28 09:46:10 +0700

    adjust

--- app/users/routes/userRoutes.js
+++ app/users/routes/userRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const userService = require('../services/userService');
 const crypto = require('crypto');
-const passport = require('passport');
 
 const multer = require('multer');
 const storage = multer.memoryStorage();
